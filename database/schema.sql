2263f449984d72ebe6040029c44182e519d8b93c
ImaneTech <imane.zouine100@gmail.com>
2025-11-23 15:47:53 +0100

    ajout de fichier schema pour gestion de la base de donnees

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+-- ============================================
+-- 1. Création de la base de données
+-- ============================================
+CREATE DATABASE IF NOT EXISTS gestion_frais_db
+
+
+USE gestion_frais_db;
